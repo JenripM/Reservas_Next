ecf9094f0ec1754dd2f05e1c679b6bbd6bfa6aca
JenripM <jmarrerosurquiza@gmail.com>
2024-11-24 15:55:50 -0500

    actualizado

--- src/app/api/reservas/route.ts
+++ src/app/api/reservas/route.ts
@@ -8,21 +8,18 @@ export  async function GET(){
 
 export async function POST(request: Request) {
     try {
-        // Parsear los datos de la solicitud
         const { clientName, partySize, date, status } = await request.json();
 
-        // Validar que los datos requeridos estén presentes
         if (!clientName || !partySize || !date) {
             return NextResponse.json({ error: "Faltan datos obligatorios" }, { status: 400 });
         }
 
-        // Crear una nueva reserva
         const newReservation = await prisma.reservation.create({
             data: {
                 clientName,
                 partySize,
-                date: new Date(date), // Asegúrate de convertir a Date si es necesario
-                status, // Opcional, usa el valor predeterminado si no se proporciona
+                date: new Date(date), 
+                status,
             },
         });
 
